e00b45a5a7168affc9d48ce7d96f9ff9c0b185b2
Ilmaan-Ahamed <ilmaanahamed456@gmail.com>
2026-06-22 01:10:10 +0530

    Day 2 Middlewares

--- App.js
+++ App.js
@@ -1,8 +1,22 @@
 const exp = require('express');
+
+const morgan = require('morgan')
+
 const app = exp();
 
 app.listen(3000);
 
+// // Middleware 
+// app.use((req, res, next)=>{
+//     console.log('Middleware 1')
+//     console.log(req.host);
+//     console.log(req.path);
+//     console.log(req.method)
+//     next();
+// })
+
+app.use(morgan('dev'))
+// 
 app.get('/', (req, res)=>{
     res.sendFile('./HTML_Response/index.html', {root: __dirname});
 })
@@ -19,6 +33,16 @@ app.get('/about', (req, res)=>{
     res.sendFile('./HTML_Response/about.html', {root: __dirname});
 })
 
+// app.use((req, res,next)=>{
+//     console.log('Middleware 2');
+//     next();
+// })
+
 app.use((req, res)=>{
-    res.sendFile('./HTML_Response/notfound.html' {root: __dirname} )
+    res.status(404).sendFile('./HTML_Response/notfound.html', {root: __dirname} )
 })
+
+
+// Broweer -> Request -> Server -> Response -> Browser
+
+//// Broweer -> Request -> Server {Middleware} -> Response -> Browser
